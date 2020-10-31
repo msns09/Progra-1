cc7c33c946a0cf077fe2d8535ac2a593a1dbd9ea
Santiago <Santiago@EXTRATHICCBOIE>
2020-10-31 10:32:43 +0900

    Commit ver 1.0

--- src/problema1/ProyectoProgra.java
+++ src/problema1/ProyectoProgra.java
@@ -11,8 +11,8 @@ public class ProyectoProgra {
     
     public static void main(String[] args){
         
-        boolean exitLoop = false;
-        while (!exitLoop){
+        //boolean exitLoop = false;
+       // while (!exitLoop){
             
            //utilities
            Scanner input = new Scanner(System.in);
@@ -21,24 +21,43 @@ public class ProyectoProgra {
            //variables
            double number1;
            double number2;
+           double result;
            char operator;
            
            //main code
            System.out.println("Enter numbers: ");
-           number1 = input.nextInt();
-           operator = +, -;
-           number2 = input.nextInt();
+           number1 = reader.nextDouble();
+           number2 = reader.nextDouble();
            
-           double result;
+           System.out.print("Enter operator +, -, *, /: ");
+           operator = reader.next().charAt(0);
            
-            switch(operator){
+           switch(operator){
                 case '+':
                     result = number1 + number2;
-                    
+                    break;
                    
+                case '-':
+                    result = number1 - number2;
+                    break;
+                
+                case '*':
+                    result = number1 * number2;
+                    break;
+                    
+                case '/':
+                    result = number1 / number2;
+                    break;
+                    
+                default:
+                    System.out.printf("No valid operator chosen...");
+                    return;
            }
+           
+           System.out.printf("%.1f %c %.1f = %.1f %n", number1, operator, number2, result);
         }
-    }
+    //}
+    
 }    
 
     
