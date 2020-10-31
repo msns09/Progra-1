0a11976efe5d75ca29cb65d6513f33f9c34377bf
Santiago <Santiago@EXTRATHICCBOIE>
2020-10-31 09:28:08 +0900

    Commit ver 1.0

---
+++ src/proyecto/progra/ProyectoProgra.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.progra;
+
+import java.util.Scanner;
+
+public class ProyectoProgra {
+    
+    public static void main(String[] args){
+        
+        boolean exitLoop = false;
+        while (!exitLoop){
+            
+           //utilities
+           Scanner input = new Scanner(System.in);
+           Scanner reader = new Scanner(System.in);
+           
+           //variables
+           double number1;
+           double number2;
+           String operator;
+           
+           //main code
+           System.out.println("Enter numbers: ");
+           number1 = input.nextInt();
+           operator = input.next();
+           number2 = input.nextInt();
+           
+           double result;
+           
+            switch(operator){
+               case '+':
+                   
+           }
+        }
+    }
+}    
+
+    
