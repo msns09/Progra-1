f28bb85dfbbc45d61d92dc32ddca3a2503c9f22e
Santiago <>
2020-10-31 10:56:53 +0900

    Commit ver 1.0

--- src/problema1/ProyectoProgra.java
+++ src/problema1/ProyectoProgra.java
@@ -8,54 +8,51 @@ package problema1;
 import java.util.Scanner;
 
 public class ProyectoProgra {
-    
-    public static void main(String[] args){
-        
-        //boolean exitLoop = false;
-       // while (!exitLoop){
-            
-           //utilities
-           Scanner input = new Scanner(System.in);
-           Scanner reader = new Scanner(System.in);
-           
-           //variables
-           double number1;
-           double number2;
-           double result;
-           char operator;
-           
-           //main code
-           System.out.println("Enter numbers: ");
-           number1 = reader.nextDouble();
-           number2 = reader.nextDouble();
-           
-           System.out.print("Enter operator +, -, *, /: ");
-           operator = reader.next().charAt(0);
-           
-           switch(operator){
+
+    public static void main(String[] args) {
+
+        boolean exitLoop = false;
+        while (!exitLoop) {
+            //utilities
+            Scanner input = new Scanner(System.in);
+            Scanner reader = new Scanner(System.in);
+
+            //variables
+            double number1;
+            double number2;
+            double result;
+            char operator;
+
+            //main code
+            System.out.println("Enter numbers: ");
+            number1 = reader.nextDouble();
+            number2 = reader.nextDouble();
+
+            System.out.print("Enter operator +, -, *, /: ");
+            operator = reader.next().charAt(0);
+
+            switch (operator) {
                 case '+':
                     result = number1 + number2;
                     break;
-                   
+
                 case '-':
                     result = number1 - number2;
                     break;
-                
+
                 case '*':
                     result = number1 * number2;
                     break;
-                    
+
                 case '/':
                     result = number1 / number2;
                     break;
-                    
+
                 default:
                     System.out.printf("No valid operator chosen...");
                     return;
-           }
-           
-           System.out.printf("%.1f %c %.1f = %.1f %n", number1, operator, number2, result);
-        }   
-}    
-
-    
+            }
+            System.out.printf("%.1f %c %.1f = %.1f %n", number1, operator, number2, result);
+        }
+    }
+}
